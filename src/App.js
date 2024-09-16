b3e6dc6bf846c20e8669166bf194d83e38f9148c
Jan Moritz Pocher <janmoritz@pocher.de>
2024-09-16 19:05:00 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
 import { AppBar, Box, Button, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 
 function App() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
     <div className="App">
       <Box>
 
       <AppBar position='static'>
         <Toolbar>
-          <Menu>
+          <Menu open={anchorElNav} anchorEl={anchorElNav} onClose={handleCloseNavMenu}>
             <MenuItem>
-            <Typography>Test</Typography>
+              <Typography>Test</Typography>
             </MenuItem>
           </Menu>
           </Toolbar>
