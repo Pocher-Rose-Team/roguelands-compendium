5e42bcbfcfe4b8b41fb9323bfe8c2010d22d385a
corvin-rose <business@corvin-rose.de>
2024-10-04 21:57:22 +0200

    Add carticon svg

--- src/root/shopping-cart/shopping-cart-icon.tsx
+++ src/root/shopping-cart/shopping-cart-icon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ShoppingCartIcon() {
-  return <img src="/img/ui/carticon.png" alt="Cart Icon" style={{ width: 24, padding: 3 }} />;
+  return <img src="/img/ui/carticon.svg" alt="Cart Icon" style={{ width: 24, padding: 3 }} />;
 }
